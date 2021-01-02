1ca33fb6d7c9cb1e608a5aa06af1e562d835b617
Felipe Martins <50122036+felipemvitor@users.noreply.github.com>
2021-01-02 05:37:57 -0300

    Feign Client

--- hr-payroll/src/main/kotlin/com/felipe/hrpayroll/service/PaymentService.kt
+++ hr-payroll/src/main/kotlin/com/felipe/hrpayroll/service/PaymentService.kt
@@ -2,6 +2,7 @@ package com.felipe.hrpayroll.service
 
 import com.felipe.hrpayroll.entities.Payment
 import com.felipe.hrpayroll.entities.Worker
+import com.felipe.hrpayroll.feignclients.WorkerFeignClient
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
@@ -10,17 +11,12 @@ import org.springframework.web.client.RestTemplate
 @Service
 class PaymentService {
 
-    @Value("\${hr-worker.host}")
-    private lateinit var workerHost: String
 
     @Autowired
-    private lateinit var restTemplate: RestTemplate
+    private lateinit var workerFeignClient: WorkerFeignClient
 
     fun getPayment(workerId: Long, days: Int): Payment {
-        val uriVariables = HashMap<String, String>()
-        uriVariables["id"] = workerId.toString()
-
-        val worker: Worker? = restTemplate.getForObject("$workerHost/workers/{id}", Worker::class.java, uriVariables)
+        val worker: Worker? = workerFeignClient.findById(workerId).body
 
         return Payment(worker?.name, worker?.dailyIncome, days)
     }

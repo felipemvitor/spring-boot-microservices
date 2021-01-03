4b16a840f3e410e491a40222226cac3d4982bbba
Felipe Martins <50122036+felipemvitor@users.noreply.github.com>
2021-01-03 15:29:55 -0300

    Projet hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/kotlin/com/felipe/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.felipe.hrapigatewayzuul
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}

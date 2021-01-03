4ea6f5a2267668ce017961048d3262c137fa6df8
Felipe Martins <50122036+felipemvitor@users.noreply.github.com>
2021-01-03 14:39:10 -0300

    Hystrix fault tolerance

--- hr-payroll/src/main/kotlin/com/felipe/hrpayroll/resources/PaymentResource.kt
+++ hr-payroll/src/main/kotlin/com/felipe/hrpayroll/resources/PaymentResource.kt
@@ -2,6 +2,7 @@ package com.felipe.hrpayroll.resources
 
 import com.felipe.hrpayroll.entities.Payment
 import com.felipe.hrpayroll.service.PaymentService
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,8 +17,14 @@ class PaymentResource {
     @Autowired
     private lateinit var service: PaymentService
 
+    @HystrixCommand(fallbackMethod = "getPaymentAlternative")
     @GetMapping("/{workerId}/days/{days}")
     fun getPayment(@PathVariable workerId: Long, @PathVariable days: Int): ResponseEntity<Payment> {
         return ResponseEntity.ok(service.getPayment(workerId, days))
+    }
+
+    fun getPaymentAlternative(workerId: Long, days: Int): ResponseEntity<Payment> {
+        val payment = Payment("Brann", 400.0, days)
+        return ResponseEntity.ok(payment)
     }
 }

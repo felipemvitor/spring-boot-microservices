c1d27d55dd1185c42a24e52b0ef09781fcf50b9a
Felipe Martins <50122036+felipemvitor@users.noreply.github.com>
2021-01-03 23:04:04 -0300

    Configuring hr-config-server project

---
+++ hr-config-server/src/main/kotlin/com/felipe/hrconfigserver/HrConfigServerApplication.kt
@@ -0,0 +1,13 @@
+package com.felipe.hrconfigserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@EnableConfigServer
+@SpringBootApplication
+class HrConfigServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<HrConfigServerApplication>(*args)
+}

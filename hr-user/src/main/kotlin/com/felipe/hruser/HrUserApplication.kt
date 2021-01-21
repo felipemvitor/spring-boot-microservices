f98c717bab059dc5bdf5c025b2dd10fd0023720e
Felipe Martins <50122036+felipemvitor@users.noreply.github.com>
2021-01-21 00:33:33 -0300

    Database seeding

--- hr-user/src/main/kotlin/com/felipe/hruser/HrUserApplication.kt
+++ hr-user/src/main/kotlin/com/felipe/hruser/HrUserApplication.kt
@@ -1,12 +1,23 @@
 package com.felipe.hruser
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 
 @EnableEurekaClient
 @SpringBootApplication
-class HrUserApplication
+class HrUserApplication : CommandLineRunner {
+
+    @Autowired
+    private lateinit var passwordEncoder: BCryptPasswordEncoder
+
+    override fun run(vararg args: String?) {
+        println("BCRYPT = ${passwordEncoder.encode("123456")}")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<HrUserApplication>(*args)

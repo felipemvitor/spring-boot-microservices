2d438e8c82aa36c7ed5275e51182aad2420da630
Felipe Martins <50122036+felipemvitor@users.noreply.github.com>
2021-01-20 23:51:58 -0300

    Configuring hr-user project

--- hr-user/src/main/kotlin/com/felipe/hruser/HrUserApplication.kt
+++ hr-user/src/main/kotlin/com/felipe/hruser/HrUserApplication.kt
@@ -2,7 +2,9 @@ package com.felipe.hruser
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
+@EnableEurekaClient
 @SpringBootApplication
 class HrUserApplication
 

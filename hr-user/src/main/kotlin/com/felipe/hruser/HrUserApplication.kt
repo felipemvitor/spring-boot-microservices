b53be72bc84a3a26ecccf1413b557e0e9878b4b9
Felipe Martins <50122036+felipemvitor@users.noreply.github.com>
2021-01-20 23:49:50 -0300

    Project hr-user created

---
+++ hr-user/src/main/kotlin/com/felipe/hruser/HrUserApplication.kt
@@ -0,0 +1,11 @@
+package com.felipe.hruser
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrUserApplication
+
+fun main(args: Array<String>) {
+    runApplication<HrUserApplication>(*args)
+}
